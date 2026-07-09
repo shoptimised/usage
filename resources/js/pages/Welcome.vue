6fd5c61c10572e3266cdab1c6d3175b3418e2578
David Zborowski <david@shoptimised.com>
2026-07-09 18:01:32 +0100

    remove registration

--- resources/js/pages/Welcome.vue
+++ resources/js/pages/Welcome.vue
@@ -1,7 +1,6 @@
 <script setup lang="ts">
 import { Head, Link } from '@inertiajs/vue3';
 import { dashboard, login } from '@/routes';
-import { register } from '@/routes';
 </script>
 
 <template>
@@ -29,12 +28,6 @@ import { register } from '@/routes';
                         class="inline-block rounded-sm border border-transparent px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#19140035] dark:text-[#EDEDEC] dark:hover:border-[#3E3E3A]"
                     >
                         Log in
-                    </Link>
-                    <Link
-                        :href="register()"
-                        class="inline-block rounded-sm border border-[#19140035] px-5 py-1.5 text-sm leading-normal text-[#1b1b18] hover:border-[#1915014a] dark:border-[#3E3E3A] dark:text-[#EDEDEC] dark:hover:border-[#62605b]"
-                    >
-                        Register
                     </Link>
                 </template>
             </nav>
